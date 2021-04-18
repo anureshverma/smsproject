37ebb97d136f3c4f5cd7f5ce9a60b3b9beced838
Anuresh Verma <32224679+anureshverma@users.noreply.github.com>
2021-04-18 18:16:13 +0530

    Update config.js
    removed expired twilio token

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const config = {
     port: 3000,
     kafkaHost: 'localhost:9092',
     //kafkaTopic: 'testTopic1',
-    twilioAccountSid: 'AC283aeb723c4e4929bd9bb6b4fa10b35a',
-    twilioAuthToken: 'b676669a733461670a40d438b50b0476',
+    twilioAccountSid: '',
+    twilioAuthToken: '',
     twilioNumber: '+13344633171',
     mongoDbUrl: 'mongodb://localhost/sms_database_dev', //sms_database_dev is db name
     htmlFileToConvertInPdf: '/home/anuresh/Downloads/test.html',
